0c9bb4f67443ac8b319b409e2e1d5d3f1c365d8d
Nabin Pandey <navin417054@gmail.com>
2025-12-02 10:33:25 +0545

    Pushed Vehicle Queue Logic.

--- src/main/java/com/traffic/core/VehicleQueue.java
+++ src/main/java/com/traffic/core/VehicleQueue.java
@@ -1,4 +1,68 @@
 package com.traffic.core;
 
 public class VehicleQueue {
+    private CarNode front ;
+    private CarNode rear;
+    private int size;
+
+    public VehicleQueue() {
+        this.front = null ;
+        this.rear = null ;
+        this.size = 0;
+
+    }
+
+    public CarNode getFront(){
+        return front;
+    }
+    public CarNode getRear(){
+        return rear;
+    }
+    public int getSize(){
+        return size ;
+    }
+    public void setSize(int size){
+        this.size = size ;
+        return ;
+    }
+
+    public void enqueue(String vehicleId){
+        CarNode newNode = new CarNode(vehicleId) ;
+
+        if(rear == null){
+            this.front = newNode ;
+            this.rear = newNode ;
+
+        }else{
+            rear.setNext(newNode) ;
+            rear = newNode ;
+        }
+            this.size++ ;
+        System.out.println( "Vehicle : " + vehicleId + "enqueued : " + this.size);
+    }
+
+
+
+    public void dequeue(){
+
+        if(isEmpty()){
+            System.out.println("Vehicle Queue is Empty");
+            return ;
+        }
+
+        String removedID = return this.front.getVehicleId() ;
+        this.front = this.front.getNext() ;
+        this.size-- ;
+
+        System.out.println("vehicle : " + removedID + " dequeued : " + this.size);
+    }
+
+    public boolean isEmpty(){
+        return this.front == null ;
+    }
+
+
+
+
+
 }
